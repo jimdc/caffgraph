1b8cfd5360558dc6f5bdfe179bb4103a04289a30
James Carroll <jamescarroll@protonmail.ch>
2018-04-16 00:09:48 -0400

    demonstrate d3 visualization, allow time entry

--- README.md
+++ README.md
@@ -4,22 +4,24 @@ This program consists of two components:
 1. a Go program that converts a timestamped dosage of caffeine to a list of estimates for how much remains in the system 
 2. a D3.js page that visualizes in line graph form, from that Go output, how much caffeine is in the body at one time
 
+Sample output:
+![rendered from 2018-04-14T18:14:00Z,200](rendered_graph.png)
+
 # adding entries
+`halflife` and the script in `index.html` use the [comma-separated value](https://en.wikipedia.org/wiki/Comma-separated_values) (csv) format to share data, and the [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) (`go`'s `time.RFC3339`) format for date/time values within the file. 
+
 ```
 > go run halflife.go
-> Enter mg: 200
-2018-04-14T18:14 200mg
-2018-04-14T23:56 100mg
-2018-04-15T05:38 50mg
-2018-04-15T11:20 25mg
-2018-04-15T17:02 12mg
-2018-04-15T22:44 6mg
-2018-04-16T04:26 3mg
-2018-04-16T10:08 1mg
+Enter time: 
+Using default current time.
+Enter mg: 
+Using default 100mg.
+date,close
+2018-04-16T00:05:38Z,100
+2018-04-16T05:47:38Z,50
+2018-04-16T11:29:38Z,25
+2018-04-16T17:11:38Z,12
+2018-04-16T22:53:38Z,6
+2018-04-17T04:35:38Z,3
+2018-04-17T10:17:38Z,1
 ```
-
-Features to add:
-
-* output to a file (TSV, JSON) that the js can read
-* read in from file, add existing caffeine to dosage
-* webapp to write to dosage file then run halflife.go
--- halflife.go
+++ halflife.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "bufio"
     "os"
@@ -9,23 +10,72 @@ import (
     "time"
 )
 
-func main() {
-    const halflife = 5.7
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func getEntryTime() (t time.Time, err error) {
     reader := bufio.NewReader(os.Stdin)
-    t := time.Now()
+
+    fmt.Print("Enter time: ")
+    str, _ := reader.ReadString('\n')
+    str = strings.TrimSpace(str)
+
+    if (str != "") {
+        t1, e := time.Parse(time.RFC3339, str)
+        if e != nil {
+            return time.Time{}, e
+        }
+
+        return t1, nil
+    }
+
+    fmt.Println("Using default current time.")
+    return time.Now(), nil
+}
+
+func getEntryMg() (mg int64, err error) {
+    reader := bufio.NewReader(os.Stdin)
 
     fmt.Print("Enter mg: ")
     str, _ := reader.ReadString('\n')
-    mg, err := strconv.ParseInt(strings.TrimSpace(str), 10, 64)
+    str = strings.TrimSpace(str)
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
+    if (str != "") {
+        userMg, e := strconv.ParseInt(strings.TrimSpace(str), 10, 64)
+        if e != nil {
+            return -1, e
+        }
+
+        return userMg, nil
     }
 
+    fmt.Println("Using default 100mg.")
+    return 100, nil
+}
+
+func main() {
+    writePtr := flag.Bool("write", false, "write csv output to caff.csv")
+    flag.Parse()
+
+    t, err := getEntryTime()
+    check(err)
+
+    mg, err := getEntryMg()
+    check(err)
+
+    if *writePtr == true {
+        f, err := os.Create("caffeine.csv")
+        check(err)
+        defer f.Close()
+    }
+
+    fmt.Println("date,close");
     for inmg := mg; inmg >= 1; inmg /= 2 {
-        fmt.Printf("%d-%02d-%02dT%02d:%02d %dmg\n",
-            t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), inmg)
+        fmt.Printf("%d-%02d-%02dT%02d:%02d:%02dZ,%d\n",
+            t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), inmg)
 
         //The halflife of caffeine is 5.7 hours, aka 5 hours and 42 minutes
         t = t.Add(time.Hour * 5 + time.Minute * 42)
