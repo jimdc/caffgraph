d83f59ec9ee612a7d08008c9f5407c633fec4d06
James Carroll <jamescarroll@protonmail.ch>
2018-04-16 03:23:42 -0400

    -write switch and use channels for halflife

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Sample output:
 ![rendered from 2018-04-14T18:14:00Z,200](rendered_graph.png)
 
 # adding entries
-`halflife` and the script in `index.html` use the [comma-separated value](https://en.wikipedia.org/wiki/Comma-separated_values) (csv) format to share data, and the [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) (`go`'s `time.RFC3339`) format for date/time values within the file. 
+`halflife` and the script in `index.html` use the [comma-separated value](https://en.wikipedia.org/wiki/Comma-separated_values) (csv) format to share data, and the [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) (`go`'s `time.RFC3339`) format for date/time values within the file. The `-write` switch writes to `caffeine.csv` instead of to stdout, as in the example:
 
 ```
 > go run halflife.go
@@ -25,3 +25,8 @@ date,close
 2018-04-17T04:35:38Z,3
 2018-04-17T10:17:38Z,1
 ```
+
+# future
+1. csv -> JSON, to include `remnants` as an array inside `dosage`s (which have `name`s)
+2. make halflife parameter more generic, to enable not just caff
+3. go web server
--- halflife.go
+++ halflife.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "math"
     "flag"
     "fmt"
     "bufio"
@@ -56,9 +57,35 @@ func getEntryMg() (mg int64, err error) {
     return 100, nil
 }
 
+type remnant struct {
+    date string
+    amount int64
+}
+
+// math.Round doesn't round up, so compensate when allocating
+func nHalves(mg int64) (timesToDivide int64) {
+   fmg := float64(mg)
+   nDivides := math.Round(math.Log2(fmg))
+   return int64(nDivides)
+}
+
+func calculateHl(t time.Time, mg int64, remnants chan remnant) {
+    for inmg := mg; inmg >= 1; inmg /= 2 {
+        formatted := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02dZ",
+            t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+
+        remnants <- remnant { date: formatted, amount: inmg }
+
+        //The halflife of caffeine is 5.7 hours, aka 5 hours and 42 minutes
+        t = t.Add(time.Hour * 5 + time.Minute * 42)
+    }
+    close(remnants)
+}
+
 func main() {
-    writePtr := flag.Bool("write", false, "write csv output to caff.csv")
+    writePtr := flag.Bool("write", false, "write csv output to caffeine.csv")
     flag.Parse()
+    var f *os.File
 
     t, err := getEntryTime()
     check(err)
@@ -67,17 +94,25 @@ func main() {
     check(err)
 
     if *writePtr == true {
-        f, err := os.Create("caffeine.csv")
+        f, err = os.Create("caffeine.csv")
         check(err)
+        _, err2 := f.WriteString("date,close\n")
+        check(err2)
         defer f.Close()
+    } else {
+        fmt.Println("date,close")
     }
 
-    fmt.Println("date,close");
-    for inmg := mg; inmg >= 1; inmg /= 2 {
-        fmt.Printf("%d-%02d-%02dT%02d:%02d:%02dZ,%d\n",
-            t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), inmg)
-
-        //The halflife of caffeine is 5.7 hours, aka 5 hours and 42 minutes
-        t = t.Add(time.Hour * 5 + time.Minute * 42)
+    bufSize := nHalves(mg) + 1
+    c := make(chan remnant, bufSize)
+    go calculateHl(t, mg, c)
+    for remy := range c {
+        csv := fmt.Sprintf("%s,%d\n", remy.date, remy.amount)
+        if *writePtr == true {
+            _, err3 := f.WriteString(csv)
+            check(err3)
+        } else {
+            fmt.Printf(csv)
+        }
     }
 }
