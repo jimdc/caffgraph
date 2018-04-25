df92204bcc3d039073de5ed17b1fe62e68d54e94
James Carroll <jamescarroll@protonmail.ch>
2018-04-24 23:43:44 -0400

    os.Getenv(PORT) for http.ListenAndServe for hosting service

--- server/Main.go
+++ server/Main.go
@@ -3,11 +3,21 @@ package main
 import (
     "log"
     "net/http"
+    "os"
+    "fmt"
 )
 
+func determineListenAddress() (string, error) {
+  port := os.Getenv("PORT")
+  if port == "" {
+    return ":8080", fmt.Errorf("$PORT not set")
+  }
+  return ":" + port, nil
+}
+
 func main() {
-
     router := NewRouter()
+    addr, _ := determineListenAddress()
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+    log.Fatal(http.ListenAndServe(addr, router))
 }
